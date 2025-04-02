e89b65cd39e712ce26d4162623ef705e121de3b6
João Vitor <joao.vitor@fengbrasil.com.br>
2025-04-02 02:11:34 -0300

    gitflow-feature-stash: PRESENTABLE-VERSION

--- src/main/java/vigoraes/hubspot_integration/controllers/AuthController.java
+++ src/main/java/vigoraes/hubspot_integration/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package vigoraes.hubspot_integration.controllers;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,28 +17,37 @@ import vigoraes.hubspot_integration.services.AuthService;
 import vigoraes.hubspot_integration.utils.CacheUtils;
 
 
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    AuthController(AuthService authService) {
+    @Autowired
+    private CacheUtils cacheUtils;
+
+    AuthController(AuthService authService, CacheUtils cacheUtils) {
         this.authService = authService;
+        this.cacheUtils = cacheUtils;
     }
 
 
-    @GetMapping("/initialize-oauth")
+    @GetMapping("/oauth")
     public  ResponseEntity<String> initializeOauth(HttpServletResponse res) throws IOException{
         return authService.initializeOauth(res);
     }
 
-    @GetMapping("/callback")
-    public ResponseEntity<String> handleOAuthCallback(@RequestParam("code") String code) {
+    @GetMapping("/login")
+    public ResponseEntity<String> login(@RequestParam("code") String code) {
 
-        CacheUtils.cacheOAuthCode(code);
-        
-        return ResponseEntity.ok("Código recebido: " + code);
+        if(code == null){
+            throw new RuntimeException("Código retornado é nulo.");
+        }
+
+        authService.getAccessTokenAndRefreshToken(code);
+
+        return ResponseEntity.ok("Login realizado.");
     }
     
 
@@ -45,13 +56,18 @@ public class AuthController {
         return authService.createUser(authDto);
     }
     
-    @PostMapping("/get-tokens")
-    public ResponseEntity<String> getAccessTokenAndRefreshToken(@Valid @RequestBody AuthDto authDto) {
-        
-        return authService.getAccessTokenAndRefreshToken(authDto);
+    @GetMapping("/get-cache/{cache}")
+    public ResponseEntity<String> getCache(@PathVariable("cache") String cache) {
+        switch (cache) {
+            case "oauth-code":
+                return new ResponseEntity<>("Oauth Code: " + this.cacheUtils.getCachedOAuthCode() , HttpStatus.OK);
+            case "tokens":
+                return new ResponseEntity<>("Access Token: " + this.cacheUtils.getCachedTokens().toString(), HttpStatus.OK);
+            default:
+                throw new RuntimeException("Paramêtro inválido");
+        }
     }
-
-
+    
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleValidationExceptions(ConstraintViolationException ex) {
         return new ResponseEntity<>("Erro de validação: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
